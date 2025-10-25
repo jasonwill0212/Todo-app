0d0d0497ebc9b1e9a173fbe975866d36e1386e17
Sotatek-NguyenTran2 <nguyen.tran2@sotatek.com>
2025-10-25 15:59:42 +0700

    demo widgets

---
+++ lib/registration_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/main.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+        ),
+      ),
+      body: const Center(child: Text('Registration Screen')),
+    );
+  }
+}
